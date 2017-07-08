a50ca1f267b0a4b5fea7c66e6df5d0e8d1950d41
大庭　武志 <takeshi@oobatakeshi-no-MacBook-Air.local>
2017-07-08 19:31:57 +0900

    線形変換

--- main.c
+++ main.c
@@ -28,20 +28,55 @@ int main(){
 }
 
 void transform(ImageData *img,ImageData *outimg){
-    int x,y;
-    int a,b;
-    int set,get;
-    Pixel pix;
+    int x,y,i; //for文用
+    int error; //エラーチェッカー(いらないかも)
+    int max[3] = {}; //全要素0で初期化
+    int min[3] = {255,255,255};
+    Pixel pix; //r,g,b
     
-    for(x=0;x<img->width;x++){
-        for(y=0;y<img->height;y++){
-            a = getPixel(img,x,y,&pix);
-            if (a == -1){
+    //最大・最小階調を調べる
+    for(x=0;x<img->width;x++) {
+        for(y=0;y<img->height;y++) {
+            error = getPixel(img,x,y,&pix);
+            if (error == -1){
                 printf("getError(%d,%d)¥n",x,y);
+                return;
             }
-            b = setPixel(outimg,x,y,&pix);
-            if (b == -1){
+            if(max[0] < pix.r) {
+                max[0] = pix.r;
+            }
+            if(max[1] < pix.g) {
+                max[1] = pix.g;
+            }
+            if(max[2] < pix.b) {
+                max[2] = pix.b;
+            }
+            if(min[0] > pix.r) {
+                min[0] = pix.r;
+            }
+            if(min[1] > pix.g) {
+                min[1] = pix.g;
+            }
+            if(min[2] > pix.b) {
+                min[2] = pix.b;
+            }
+        }
+    }
+
+    for(x=0;x<img->width;x++) {
+        for(y=0;y<img->height;y++) {
+            error = getPixel(img,x,y,&pix);
+            if (error == -1){
+                printf("getError(%d,%d)¥n",x,y);
+                return;
+            }
+            pix.r = (int)(((double)(pix.r - min[0]) / (max[0] - min[0])) * 255);
+            pix.g = (int)(((double)(pix.g - min[1]) / (max[1] - min[1])) * 255);
+            pix.b = (int)(((double)(pix.b - min[2]) / (max[2] - min[2])) * 255);
+            error = setPixel(outimg,x,y,&pix);
+            if (error == -1) {
                 printf("setError(%d,%d)¥n",x,y);
+                return;
             }
         }
     }
